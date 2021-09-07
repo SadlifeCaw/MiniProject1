de312c3bd3226284f5ad6ffd2adaca428ecc0a3a
vson <vson@itu.dk>
2021-09-07 13:10:25 +0200

    Implement fork and channels

--- source/Fork.go
+++ source/Fork.go
@@ -5,3 +5,34 @@ import "fmt"
 func printFork() {
 	fmt.Println("I am a fork")
 }
+
+type Fork struct {
+	inUse     bool
+	timesUsed int
+	in        chan (bool)
+	out       chan (bool)
+}
+
+func newFork() *Fork {
+	in := make(chan bool)
+	out := make(chan bool)
+
+	f := Fork{inUse: false, timesUsed: 1, in: in, out: out}
+
+	return &f
+}
+
+func occupy(f *Fork) {
+	f.in <- true
+}
+
+func unoccupy(f *Fork) {
+	f.in <- false
+}
+
+func activateChannels(f *Fork) {
+	message := <-f.in
+	fmt.Println(message, "hej")
+
+	go activateChannels(f)
+}
--- source/Main.go
+++ source/Main.go
@@ -1,6 +1,14 @@
 package main
 
+//import "fmt"
+
 func main() {
-	printPhilosopher()
-	printFork()
+	Fork1 := newFork()
+
+	go activateChannels(Fork1)
+
+	occupy(Fork1)
+
+	//msg := <-Fork1.in
+	//fmt.Println(msg)
 }
