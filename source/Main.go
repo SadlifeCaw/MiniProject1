3cb9d1b7800176fa7edc0bda433c045d18c69c11
Stalin1999 <90043237+Stalin1999@users.noreply.github.com>
2021-09-14 13:30:36 +0200

    Implemented working queries via channels (#8)

--- source/Main.go
+++ source/Main.go
@@ -9,26 +9,43 @@ func main() {
 
 	var forks = make([]*fork, 5)
 	for j := 0; j < 5; j++ {
-		var thisfork *fork = new(fork)
-		thisfork.id = j
-		forks[j] = new(fork)
+		forks[j] = newFork(j)
 	}
 	var philosophers = make([]*philosopher, 5)
 	for j := 0; j < 5; j++ {
-		var phil *philosopher = new(philosopher)
-		phil.id = j
-		phil.leftFork = forks[j]
-		phil.rightFork = forks[(j+1)%5]
-		philosophers = append(philosophers, phil)
-		go phil.eat()
+		philosophers[j] = newPhil(j, forks[j], forks[(j+1)%5])
 	}
-	fmt.Scanln()
-	go reader()
-}
-
-func reader(){
-	time.Sleep(time.Millisecond * 5000)
-	var request string
-	fmt.Scan(&request)
-	//Implement different requests
+	
+	for {
+		fmt.Println("Sending queries:")
+		for i := 0; i < 5; i++ {
+			time.Sleep(time.Millisecond * 1464)
+			forks[i].in <- -1
+			inUse := <-forks[i].out
+			var inUseString string
+			if (inUse == 1){
+				inUseString = "Being used"
+			} else {
+				inUseString = "Not being used"
+			}
+			forks[i].in <- -2
+			timesUsed := <-forks[i].out
+			fmt.Println("Fork", i+1, ":", inUseString, "and has been used:", timesUsed, "times.")
+		} 
+		time.Sleep(time.Millisecond * 2000)
+		for i := 0; i < 5; i++ {
+			time.Sleep(time.Millisecond * 1464)
+			philosophers[i].in <- -1
+			eating := <-philosophers[i].out
+			var eatingString string
+			if (eating == 1){
+				eatingString = "Eating"
+			} else {
+				eatingString = "Thinking"
+			}
+			philosophers[i].in <- -2
+			bites := <-philosophers[i].out
+			fmt.Println("Philosopher", i+1, ":", eatingString, "and has eaten: ", bites, "bites.")			
+		} 
+	}
 }
